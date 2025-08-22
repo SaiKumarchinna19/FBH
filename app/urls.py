670a43d8401f7bf1fe513417781adc47e4cb8598
Saikumar Bodepelly <chinnasaikumar24@gmail.com>
2025-08-22 13:06:37 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -7,8 +7,10 @@ urlpatterns=[
     path('3',views.valid_otp,name='valid_otp'),
     path('4',views.deposite,name='deposite'),
     path('5',views.bal,name='bal'),
+    
     path('6',views.withdraw,name='withdraw'),
     path("7",views.transfer,name='transfer')
 
 ]
+
  
